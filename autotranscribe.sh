d94c535dafe6e2e22a0dc53b37aa376612487090
oluwaseyi <owoeye.g.o@gmail.com>
2018-05-23 15:00:32 +0100

    extract a few configurable parameters to variables

--- autotranscribe.sh
+++ autotranscribe.sh
@@ -10,30 +10,33 @@
 
 date=`date +%s`
 request_ID="transcribe_job_$date"
+audio_length=14
+aws_s3_bucket="seyi-bucket001"
+aws_region="us-east-2"
+
 # 1. Record audio. Max limit of 14 seconds. Amazon has actual 2-hour limit
-echo "Recording audio ..."
-arecord -d 14 -f S16_LE speech.wav
+echo "Recording $audio_length seconds of audio ..."
+arecord -d ${audio_length} -f S16_LE speech.wav
 echo "Recording stopped"
 
 # 2. Upload audio file to s3
 echo "Uploading audio ..."
-aws s3 cp speech.wav "s3://seyi-bucket001/$request_ID.wav"
+aws s3 cp speech.wav "s3://$aws_s3_bucket/$request_ID.wav"
 
 # 3. Use s3 audio to create and start a Transcribe job
-transcribe_job_1527079214.wav
 echo "Starting transcription ..."
 
 echo "{
     \"TranscriptionJobName\": \"$request_ID\",
     \"LanguageCode\": \"en-US\",
     \"MediaFormat\": \"wav\",
-    \"Media\": {\"MediaFileUri\": \"https://s3.us-east-2.amazonaws.com/seyi-bucket001/test1.wav\"}
+    \"Media\": {\"MediaFileUri\": \"https://s3.$aws_region.amazonaws.com/$aws_s3_bucket/$request_ID.wav\"}
 }" > request.json
-aws transcribe start-transcription-job --region us-east-2 --cli-input-json file://request.json
+aws transcribe start-transcription-job --region ${aws_region} --cli-input-json file://request.json
 
 # 4. Keep checking Transcribe job till its COMPLETED
 echo "Transcription $request_ID in progress ..."
-aws transcribe get-transcription-job --region us-east-2 --transcription-job-name $request_ID > response.json
+aws transcribe get-transcription-job --region ${aws_region} --transcription-job-name $request_ID > response.json
 x=1
 while [ $x -le 2 ]
 do
@@ -43,7 +46,7 @@ do
       x=3
   else
       sleep 5  # 5 seconds sleep before trying again
-      aws transcribe get-transcription-job --region us-east-2 --transcription-job-name $request_ID > response.json
+      aws transcribe get-transcription-job --region ${aws_region} --transcription-job-name $request_ID > response.json
   fi
 done
 echo "Transcription done."
